4817dfed2722359ed2ce9531c1ffef9754cb9b60
Adnane El yazidi <adanyazi22@outlook.com>
2025-09-22 17:20:16 +0100

    vch2v1

---
+++ Day01/01-Variables.c/Challenge_2.c
@@ -0,0 +1,21 @@
+/*Challenge 2 : Conversion de la température
+Écrivez un programme qui demande la température en Celsius et la 
+transforme en Kelvin. Formule :
+
+K = C + 273.15*/
+
+#include <stdio.h>
+
+int main()
+{
+    float K, C;
+
+    printf("Entrer la températur en Celsius: ");
+    scanf("%f", &C);
+
+    K = C + 273.15;
+
+    printf("la temperature en Kelvin est: %.2f\n", K);
+
+    return 0;
+}
