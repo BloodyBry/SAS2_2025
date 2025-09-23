985b2a69489126fbfb7e6de75feb335ffcab7a5c
Adnane El yazidi <adanyazi22@outlook.com>
2025-09-23 09:24:46 +0100

    vch5v1

---
+++ Day01/01-Variables.c/Challenge_5.c
@@ -0,0 +1,28 @@
+/*Challenge 5 : Affichage Température
+Écrivez un programme qui demande la température en Celsius et 
+affiche l'état de l'eau à cette température (solide, liquide, gaz). 
+Règle :
+
+C < 0 : Solide
+0 <= C < 100 : Liquide
+C >= 100 : Gaz
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    float C;
+
+    printf("entrer la temperature en Celsius: ");
+    scanf("%f", &C);
+
+    if (C < 0)
+        printf("Solide\n");
+    else if (C >= 0 && C < 100)
+        printf("Liquide\n");
+    else
+        printf("Gaz\n");
+
+    return 0;
+}
